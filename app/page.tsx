3d30667535085f040303140af526a61077206a1d
Your Name01~ <brenoleitepereira@gmail.com>
2023-05-09 18:47:45 -0300

    Fix: correção de link do button

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,11 @@ export default function Home() {
       <h2 className="text-3xl	text-center">Selecione o destino</h2>
 
       <div className="flex flex-col gap-8 min-w-full">
-        <button className="min-w-full bg-gradient-to-r from-indigo-500 p-3 rounded-md text-xl">
-          <Link href="/causas">Causas</Link>
-        </button>
+        <Link href="/causas">
+          <button className="min-w-full bg-gradient-to-r from-indigo-500 p-3 rounded-md text-xl">
+            Causas
+          </button>
+        </Link>
         <button className="bg-gradient-to-r from-indigo-500 p-3 rounded-md text-xl">
           Causas
         </button>
